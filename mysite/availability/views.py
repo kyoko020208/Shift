2dde09fe2b1258ee586f67c6aa043850a7530403
kyoko020208 <kyoko020208@gmail.com>
2018-11-20 00:12:50 -0800

    Innitial update

--- mysite/availability/views.py
+++ mysite/availability/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django,contrib,auth import login as auth_login
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.views import View
 
-# Create your views here.
+from .forms import LoginForm
+
+class LoginView(View):
+    def get(self, request, *args, **kwargs):
+        """
+        Method for getting Request
+        """
+
+        context = {
+            'form': LoginForm(),
+        }
+
+        #return unfilled form in login template
+        return render(request, 'accounts/login.html', context)
+
+    def post(self, request, *args, **kwargs):
+        """Method for getting POST"""
+
+        #Create form
+        form = LoginForm(request.POST)
+
+        #Validation
+        if not form.is_valid():
+            #if validation fails, show the following message
+            return render(request, 'accounts/login.html',{'form' : form})
+
+        #Get User Object
+        user = form.get_user()
+
+        #login
+        auth_login(request, user)
+
+        #Redirect
+        return redirect(reverse('shop:index'))
+
+login = LoginView.as_view()
